fd60c20eba269f38c7b4d7ef970b37f8eddf371c
zk2ly <377351842@qq.com>
2021-01-22 11:46:07 +0800

    c++

---
+++ c++数据结构/data.h
@@ -0,0 +1,13 @@
+
+#ifndef DATA
+#define DATA
+
+#include <string>
+
+struct data{
+    std::string bookNo;
+    unsigned sum = 0;
+    double price = 0.0;
+};
+
+#endif
--- c++数据结构/test.cpp
+++ c++数据结构/test.cpp
@@ -1,67 +1,16 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include "data.h"
 
 using namespace std;
 
-class sparseGraph{
 
-private:
-    int v, e;  // 记录结点和边的数量
-    bool directed;  // 是否是有向图
-    vector<vector<bool>> g;  // 邻接矩阵
-
-public:
-    sparseGraph(int n, bool directed){
-        assert(n>0);
-
-        this->v=n;
-        this->e=0;
-        this->directed = directed;
-        this->g = vector<vector<bool>>(n, vector<bool>());  // g初始化为n个空的vector, 表示每一个g[i]都为空, 即没有任和边
-    }
-
-    ~sparseGraph(){}
-
-    // 图中的节点数
-    int numV(){
-        return v;
-    }
-
-    // 图中的边数
-    int numE(){
-        return e;
-    }
-
-    // 判断是否有p到q的连接
-    bool hasE(int p, int q){
-        assert(p>0 && p<=v && q>0 && q<=v);  // 确定没有越界
-
-        for(int i=0; i<g[p].size(); i++)
-            if(g[p][i] == q)
-                return true;
-        return false;
-    }
-
-    // 添加p到q的边
-    void addE(int p, int q){
-        assert(p>0 && p<=v && q>0 && q<=v);
-
-        if(hasE(p,q))
-            return;
-
-        g[p].push_back(q);  // 添加边
-
-        if(!directed)
-            g[q].push_back(p);
-
-        e++;
-    }
-};
-
-
-
-int main(){
+int main()
+{
+    data d1;
+    cout<<d1.sum<<endl;
 
     return 0;
 }
+
