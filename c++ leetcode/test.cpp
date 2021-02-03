b1724f63ae7331847c9b67a6762ac22aa763b6cb
zk2ly <377351842@qq.com>
2021-02-03 10:57:52 +0800

    lc

--- c++ leetcode/test.cpp
+++ c++ leetcode/test.cpp
@@ -5,48 +5,52 @@
 
 using namespace std;
 
+
 class Solution {
-private:
-    int need[128];
 public:
+    // 窗口[l,r)
+    // 当前元素s[r] 加入窗口
+    // 判断还缺不缺t中的元素 如果不缺  缩减窗口大小  要保证始终包含所有t的字符
+    // 记录此时的开始长度和大小
+    // 踢出左侧元素 继续循环
     string minWindow(string s, string t) {
-        // 此时窗口需要的字符和需要的个数
-        for(string::size_type i =0; i<t.szie();i++)
-            need[t[i]]++;
+        int l=0, r =0;
+        int need[128]{0};
         int needCnt = t.size();
+        int start=0, length=s.size()+1;
 
-        // 窗口[i,j]  
-        int i=0,j=-1;
-        int len=s.size()-1, start = 0;
-        while(j<s.size()){
-            while(!needCnt && i+1<s.size()){  // 没有需要的元素了 即窗口内包含所有元素 尝试去掉第一个元素
-                need[i]++;  // 去掉一个 还需要一个 如果本身是多余的 那么加进来的时侯会减成负数 此时加1 不会大于0
-                if(need[i]) needCnt++;
-                i++;
-            }
+        for(int i =0;i<t.size();i++)
+            need[t[i]]++;
 
-            if(j+1 < t.size() && need[j+1]){
-                j++;
-                need[j]--;
-                needCnt--;
-            }
-
-            if(!needCnt){
-                if((j-i+1) < num){
-                    num = j-i+1;
-                    start = i;
+        while(r<s.size()){
+            if(need[s[r]]>0) needCnt--;
+            need[s[r]]--;
+            r++;
+            if(needCnt==0){
+                while(l<r && need[s[l]] < 0){
+                    need[s[l]]++;
+                    l++;
                 }
+
+                if(r-l < length){
+                    length = r-l;
+                    start = l;
+                }
+            
+                need[s[l]]++;
+                l++;
+                needCnt++;
             }
-
         }
-
-        
+        if(length == s.size()+1)
+            return "";
+        else
+            return s.substr(start,length);
     }
 };
 
 int main(){
-    // string s = "A man, a plan, a canal: Panama";
-    // Solution().isPalindrome(s);
+    Solution().minWindow("ADOBECODEBANC", "ABC");
 
     return 0;
 }
