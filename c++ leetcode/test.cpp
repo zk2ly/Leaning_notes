275237388eb906bf1470d9e459a0bafa153674a9
zk2ly <377351842@qq.com>
2021-02-18 16:57:54 +0800

    leetcode 1 15 16 18

--- c++ leetcode/test.cpp
+++ c++ leetcode/test.cpp
@@ -1,40 +1,35 @@
 #include <iostream>
 #include <set>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    bool wordPattern(string pattern, string s) {
-        map<char, string> recode;
-        set<string> contain;
-        stringstream ss(s);
-        string str;
-
-        for(int i =0; i<pattern.size();i++){
-
-        
-            ss>>str;
-
-            if(str.empty()) return false;
-
-            char c = pattern[i];
-            if(recode.find(c)==recode.end()){
-                // if(contain.find(str) != contain.end()) return false;
-                recode[c] = str;
-                contain.insert(str);
-            }else{
-                if(recode[c] != str)
-                    return false;
+    int threeSumClosest(vector<int>& nums, int target) {
+        int res=INT_MAX;
+        int ans;
+        int i,j;
+        sort(nums.begin(),nums.end());
+        for(int k=0;k<nums.size();k++){
+            if(k>0 && nums[k]==nums[k-1]) continue;
+            i=k+1;
+            j=nums.size()-1;
+            while(i<j){
+                int sum = nums[k] + nums[i] + nums[j];
+                if(sum - target == 0) return sum;
+                if(abs(sum-target) < res){
+                    res = abs(sum-target);
+                    ans = sum;
+                }
+                if(sum > target)
+                    j--;
+                else
+                    i++;
             }
         }
-        return true;
-         
+        return ans;
     }
 };
-
-int main(){
-
-    return 0;
-}
 
